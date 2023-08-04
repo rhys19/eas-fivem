adf753ac8d3e6af003345278c826018dff5a0ab9
rhys19 <bradenweber.rx8@gmail.com>
2023-08-03 23:16:40 -0500

    Upload files

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+  'config.lua',
+  'client.lua'
+}  
+
+server_scripts {
+  'config.lua',
+  'server_config.lua',
+  'server.lua'
+}
+
+--shared_script 'config.lua'
+
+
+ui_page {
+  'index.html'
+}
+
+files {
+  'index.html',
+
+  'main.css',
+
+  'main.js',
+
+  'fonts/VCR_OSD_MONO.ttf',
+
+  'alert.mp3'
+}
+
